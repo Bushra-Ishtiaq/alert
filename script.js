ca34de054d5ef8f85aa193447eb812c266fb576c
Bushra Ishtiaq <62174610+Bushra-Ishtiaq@users.noreply.github.com>
2020-05-03 14:42:30 +0500

    added a file

---
+++ script.js
@@ -0,0 +1,18 @@
+function easyAlert(){
+    alert('This is easy level!');
+}
+function mediumAlert(){
+    alert('This is medium level!');
+}
+function hardAlert(){
+    alert('This is hard level!');
+}
+function changeStyle() {
+    document.querySelector('h1').style = 'color: red';
+}
+function removeStyle() {
+    document.querySelector('h1').style = 'color: blue';
+}
+function changeFont() {
+    document.querySelector('h1').style = 'font-size: 90px;';
+}
